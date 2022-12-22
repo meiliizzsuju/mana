cc7ba254027d6f1dde627c91502af116fee6da9c
TichaTin <ticha.tin@gmail.com>
2022-12-22 23:12:40 +1100

    Temp section added

---
+++ src/container/tempblock/TempBlock.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './TempBlock.css'
+
+const TempBlock = () => {
+  return (
+    <div className='temp_section text-center flex flex-col items-center justify-center'>
+      <span className='focus-text'>Coming Soon</span>
+      <a href="https://mana-107483.square.site/"
+        className='m-1 mx-2 btn btn-trans lg:ml-4 mt-6'
+      >
+        Booking available
+      </a>
+    </div>
+  )
+}
+
+export default TempBlock
