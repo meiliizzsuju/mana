d7fc163504c517feb8058a7701c3e5f33ad4cd4c
TichaTin <ticha.tin@gmail.com>
2022-12-22 23:22:57 +1100

    Favicon + meta tags updated

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import TempBlock from './tempblock/TempBlock';
 
 const PAGE_NAME = `page-home`;
 
 const Home = () => {
-  useEffect(() => {
-    document.title = 'Mana Dee Why';
-  }, []);
-
   return (
     <div className={PAGE_NAME}>
       <Header/>
