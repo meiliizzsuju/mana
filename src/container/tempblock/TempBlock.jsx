61aa3fe5c0aab7cdbd3923805caf509b3bff31ee
TichaTin <ticha.tin@gmail.com>
2022-12-29 14:57:43 +1100

    fix anim letter spacing issue

--- src/container/tempblock/TempBlock.jsx
+++ src/container/tempblock/TempBlock.jsx
@@ -5,7 +5,7 @@ import './TempBlock.css'
 const TempBlock = () => {
   return (
     <div className='temp_section text-center flex flex-col items-center justify-center'>
-      <span className='focus-text'>Coming Soon</span>
+      <span className='focus-text'>Coming <br className='small'/>Soon</span>
       <a href="https://mana-107483.square.site/"
         className='m-1 mx-2 btn btn-trans lg:ml-4 mt-6'
       >
