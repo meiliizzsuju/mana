b70e920f20ecbf2edc42a27cf7200d58ff08335e
Ticha Tin <62775008+meiliizzsuju@users.noreply.github.com>
2023-07-01 15:18:47 +1000

    Update copy and animation (#3)
    Co-authored-by: TichaTin <ticha@arlula.com>

--- src/components/topbanner/TopBanner.jsx
+++ src/components/topbanner/TopBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TopBanner = () => {
   return (
     <section className='promobar text-center p-1'>
-      <span>Health fund rebate is currently unavailable until mid-end of May</span>
+      <span>Health fund rebate is now available</span>
     </section>
   )
 }
