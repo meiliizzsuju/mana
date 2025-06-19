33b5d6eb10e2d9b0e13585602d394b4f4a6cd691
TichaTin <ticha@arlula.com>
2025-06-19 18:35:46 +1000

    price update

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,9 +1,9 @@
-export const services =[
+export const services = [
   {
     name: 'Traditional Thai Massage',
     slug: 'thai-massage',
     description: 'The massage that gives you refreshment and boosts your energy instantly. This dry massage will heal your body with the combination of acupressure and continuous stretching techniques. <br>Kindly let us know how firm you would like it to be, we will adjust the intensity to suit your needs.',
-    price: 'AU$80 per 60 min',
+    price: 'AU$85 per 60 min',
     presure: 'Medium to hard pressure',
     precaution: 'Due to its impact on the circulatory system, people with health conditions should consult with a GP first. Not recommended for after recent severe injury.  Not recommended for pregnant women.',
     banner_image: '/images/banner-thai.jpg',
@@ -14,7 +14,7 @@ export const services =[
     name: 'Thai Oil Massage',
     slug: 'thai-oil',
     description: 'A mix of stretching, firm pressure and deep rhythmic movements along energy lines throughout your body. This massage is similar to Traditional Thai Massage but is enhanced with the use of our special oils, resulting in a more smooth and relaxing experience. Boosts your endorphins for stress release.',
-    price: 'AU$80 per 60 min',
+    price: 'AU$85 per 60 min',
     presure: 'Light to medium pressure',
     banner_image: '/images/oil-banner.jpg',
     feature_image: '/images/oil-feature.jpg',
@@ -24,7 +24,7 @@ export const services =[
     name: 'Organic Coconut Oil & Hot Stone Massage',
     slug: 'coconut-oil',
     description: 'Down to nature, we are using organic coconut oil for this massage session. The coconut oil can be absorbed easily and act like a shield for your skin from environmental toxins after massage. Also helping to diminish the signs of ageing. Additional with hot stone which provides deep aids to your muscle.',
-    price: 'AU$85 per 60 min',
+    price: 'AU$90 per 60 min',
     presure: 'Light to medium pressure',
     banner_image: '/images/coco+hotstone.jpg',
     cover_image: '/images/hot-stone.jpg'
